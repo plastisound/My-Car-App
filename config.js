134d61c08bf8aa825c2092dff9a86f9e7de004a7
plastisound <pisd14@gmail.com>
2018-10-04 16:55:24 -0500

    All files
    loagind all files

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'mycarapp',
+		wp_ws_url : 'http://72.249.60.247/~desarrollomerca/mycarapp/wp-appkit-api/mycarapp',
+		wp_url : 'http://72.249.60.247/~desarrollomerca/mycarapp',
+		theme : 'q-android',
+		version : '1.0',
+		app_type : 'phonegap-build',
+		app_title : 'My Car App',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : -6,
+		debug_mode : 'off',
+		auth_key : '?NxCA&X=whymDy qxiDoKxZr8-up7^ia!jzkIi5F`%AA~}4dDs{U`ND-Jxbv80$t',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
